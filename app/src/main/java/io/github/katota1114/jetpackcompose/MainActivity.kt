a66a3999b9f2b224ad4ee89b1f0613b52d60674b
katota1114 <katota1114@gmail.com>
2021-07-29 13:47:50 +0900

    [add] Lesson 1: Composable functionsの実装

--- app/src/main/java/io/github/katota1114/jetpackcompose/MainActivity.kt
+++ app/src/main/java/io/github/katota1114/jetpackcompose/MainActivity.kt
@@ -14,25 +14,18 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetpackComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
+            MessageCard(name = "Android")
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
+fun MessageCard(name: String) {
     Text(text = "Hello $name!")
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
-fun DefaultPreview() {
-    JetpackComposeTheme {
-        Greeting("Android")
-    }
+fun PreviewMessageCard() {
+    MessageCard(name = "Android")
 }
